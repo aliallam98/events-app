839ebcdba70846ca19e96cf8fee1842627d2d994
aliallam98 <allam98.ali@gmail.com>
2024-01-13 19:41:41 +0200

    deployed our project on vercel .. started in create event page

---
+++ src/app/events/create/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <section>
+      <h2 className="font-semibold text-xl text-center">Create An Event</h2>
+    </section>
+  );
+};
+
+export default page;
